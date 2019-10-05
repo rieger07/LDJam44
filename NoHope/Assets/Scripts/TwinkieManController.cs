1f317f085cb3f4882f068c02e86d5d2bf63f2df6
Steve Rieger <rieger07@gmail.com>
2019-10-04 20:06:42 -0500

    added script and collisions and stuff

---
+++ NoHope/Assets/Scripts/TwinkieManController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwinkieManController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private ILogger l;
+    void Start()
+    {
+        l = GetComponent<Logger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.up);
+            
+            l.Log("Up");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left);
+            l.Log("left");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.down);
+            l.Log("left");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right);
+            l.Log("left");
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up);
+            l.Log("left");
+        }
+    }
+}
